5589b1a30e5746b18e3a8ad7e0535ad8cf3990e0
mohamedanas00 <117121756+mohamedanas00@users.noreply.github.com>
2023-04-14 12:05:10 +0200

    API_Retrive for table thecamp_cinema

--- task.js
+++ task.js
@@ -11,10 +11,34 @@ var pool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "",
-    database: "Movies Database"
+    database: "movies_database"
 })
-
-
+app.get('/cinema/movies',(req,res)=>{
+    pool.getConnection((err,conection)=>{
+      if(err){
+        console.log(err);
+      }  
+      console.log("conection ✅");
+      conection.query('SELECT * from thecamp_cinema',(err,rows)=>{
+        if(!err){
+            res.send(rows);
+        }else{
+            res.status(500).send(err);
+            console.log(err);
+        }
+      })
+    })
+})
+// app.get('/cinema/movies', (req, res) => {
+//     try {
+//         pool.query('SELECT * thecamp_cinema', function (check, rows) {
+//             res.send(rows);
+//         });
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send(err);
+//     }
+// })
 //work in port 5000
 app.listen(5000, () => {
     console.log("ServerRunning on localhost:5000✅ ....");
