a5ae20579090087b50c96433fc0157c61413b1f4
mohamedanas00 <117121756+mohamedanas00@users.noreply.github.com>
2023-04-14 12:31:30 +0200

    API Can add to thecamp_cinema

--- task.js
+++ task.js
@@ -5,6 +5,9 @@ const mysql = require("mysql");
 
 //return interface
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var pool = mysql.createPool({
     connectionLimit: 10,
@@ -13,32 +16,38 @@ var pool = mysql.createPool({
     password: "",
     database: "movies_database"
 })
-app.get('/cinema/movies',(req,res)=>{
-    pool.getConnection((err,conection)=>{
-      if(err){
-        console.log(err);
-      }  
-      console.log("conection ✅");
-      conection.query('SELECT * from thecamp_cinema',(err,rows)=>{
-        if(!err){
-            res.send(rows);
-        }else{
-            res.status(500).send(err);
+app.get('/cinema/movies', (req, res) => {
+    pool.getConnection((err, conection) => {
+        if (err) {
             console.log(err);
         }
-      })
+        console.log("conection ✅");
+        conection.query('SELECT * from thecamp_cinema', (err, rows) => {
+            if (!err) {
+                res.send(rows);
+            } else {
+                res.status(500).send(err);
+                console.log(err);
+            }
+        })
     })
 })
-// app.get('/cinema/movies', (req, res) => {
-//     try {
-//         pool.query('SELECT * thecamp_cinema', function (check, rows) {
-//             res.send(rows);
-//         });
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).send(err);
-//     }
-// })
+
+app.post('/cinema/movies', (req, res) => {
+    pool.getConnection((err, conection) => {
+        if (err) {
+            console.log(err);
+        }
+        const params = req.body;
+        conection.query('INSERT INTO thecamp_cinema SET ?', params, (err, rows) => {
+            if (!err) {
+                res.send(`Movie with Name : ${[params.movie_name]}has been added`);
+            } else {
+                console.log(err);
+            }
+        })
+    })
+})
 //work in port 5000
 app.listen(5000, () => {
     console.log("ServerRunning on localhost:5000✅ ....");
